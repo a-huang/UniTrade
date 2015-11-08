1937d4fba8cf415851787c43aebd1c0f59c99580
EllisMin <dbmin@hotmail.com>
2015-11-07 18:36:01 -0600

    initial commit

---
+++ UniTrade/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UniTrade
+//
+//  Created by Ellis on 11/7/15.
+//  Copyright © 2015 CS407. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
